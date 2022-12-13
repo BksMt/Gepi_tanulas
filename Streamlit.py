24f0d2b362cfdd8e7142a72499ce3923eec2b241
BksMt <92255225+BksMt@users.noreply.github.com>
2022-12-13 21:34:11 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,1 +1,13 @@
 import streamlit as st
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import pickle
+
+if __name__ == '__main__':
+  prec = st.number_input('Precipitation)
+  min= st.number_input('Min temp)
+  wind= st.number_input('Wind)
+  weather= st.number_input('Weather)
