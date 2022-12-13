a4b9a80eea403f42842c14fb3e050b3ebc73039d
BksMt <92255225+BksMt@users.noreply.github.com>
2022-12-13 21:41:40 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,9 +5,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import pickle
+load_model = pickle.load(open('linear_model.sav', 'rb'))
+load_scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+def weather_prediction(imput_data):
+  imput_data = load_scaler.transform(imput_data)
+  prediction = load_model.predict(imput_data)
+  return prediction
 
 if __name__ == '__main__':
-  prec = st.number_input('Precipitation)
-  min= st.number_input('Min temp)
-  wind= st.number_input('Wind)
-  weather= st.number_input('Weather)
+  prec = st.number_input('Precipitation')
+  min = st.number_input('Min temp')
+  wind= st.number_input('Wind')
+  weather= st.number_input('Weather')
+  prediction = weather_prediction([[prec, min, wind, weather]])
+  if st.button('Predict'):
+    st.success(prediction)
